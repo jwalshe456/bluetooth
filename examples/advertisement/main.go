0d3ae88fb78a336e4e2f4ca51817b1efd3c61362
Jed Walshe <jed.walshe7@mail.dcu.ie>
2025-03-29 14:45:19 +0000

    Change test code

--- examples/advertisement/main.go
+++ examples/advertisement/main.go
@@ -11,10 +11,15 @@ var adapter = bluetooth.DefaultAdapter
 func main() {
 	must("enable BLE stack", adapter.Enable())
 	adv := adapter.DefaultAdvertisement()
+	serviceUUID := bluetooth.New32BitUUID(1900)
+	userUUID := "11111900-0000-1000-8000-00805f9b34fb"
 	must("config adv", adv.Configure(bluetooth.AdvertisementOptions{
-		LocalName: "Go Bluetooth",
-		ManufacturerData: []bluetooth.ManufacturerDataElement{
-			{CompanyID: 0xffff, Data: []byte{0x01, 0x02}},
+		LocalName: "Go",
+		ServiceUUIDs: []bluetooth.UUID{
+			serviceUUID,
+		},
+		ServiceData: []bluetooth.ServiceDataElement{
+			{UUID: serviceUUID, Data: []byte(userUUID)},
 		},
 	}))
 	must("start adv", adv.Start())
